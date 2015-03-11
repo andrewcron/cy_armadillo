6fa2aac50ebb02304efdb0366202c96b81caf0c1
Andrew Cron <andrew.cron@weinraubanalytics.com>
2015-03-11 17:33:48 -0400

    added six print for py3

--- example/test_example.py
+++ example/test_example.py
@@ -1,8 +1,9 @@
 import numpy as np
 import example
+from six import print_
 
 if __name__ == "__main__":
     x = np.random.rand(20).reshape(5,4)
 
-    print example.example(x)
+    print_(example.example(x))
 
